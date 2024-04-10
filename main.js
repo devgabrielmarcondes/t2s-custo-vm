853f576387cd5e5e583c142f19e6a05aa71b9b3b
devgabrielmarcondes <devgabmasantos@gmail.com>
2024-04-10 13:50:07 -0300

    fix: favicon extension

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const resultado = document.querySelector("#resultado");
 const btn_calcular = document.querySelector("#calcular");
 btn_calcular.addEventListener("click", () => {
   const qtd_tokens = 1000000;
-  let custo_tokens = Number(tokens.value);
-  let custo_vm = Number(vm.value);
+  let custo_tokens = Number(tokens.value); // 20 tokens por segundo
+  let custo_vm = Number(vm.value); // 0.52
   let horas = qtd_tokens / custo_tokens / 3600;
   let res = custo_vm * horas;
 
-  if (custo_tokens.length !== 0 && custo_vm !== 0) {
-    resultado.innerHTML = `O custo é ${res.toFixed(2)}`;
+  if (custo_tokens.length !== 0 && custo_vm.length !== 0) {
+    resultado.innerHTML = `O custo é $${res.toFixed(2)}`;
   }
 });
